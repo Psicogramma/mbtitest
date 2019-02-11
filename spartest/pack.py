22762dc7fab277e1d7e1bdb637f23632e81329b1
Matteo Zortea <matteo.zortea29@gmail.com>
2019-02-11 19:07:38 +0100

    matteo

--- spartest/pack.py
+++
@@ -1,8 +0,0 @@
-funzioni = ('Si', 'Se', 'Ni', 'Ne', 'Ti', 'Te', 'Fi', 'Fe')
-
-def aggiornaPunteggi(risposta, func):
-    for f in funzioni:
-        if f == func:
-            print(f, func, risposta)
-
-    
